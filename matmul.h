d377c71c26ed170bfad1f69173b399ac054393fe
chaihahaha <chai836275709@gmail.com>
2024-08-20 08:08:39 +0800

    add definitions

--- matmul.h
+++ matmul.h
@@ -12,6 +12,10 @@
 
 static int matmul_float(struct float_cmat matA, struct float_cmat matB, struct float_cmat matC);
 
+static int matmul_double(struct double_cmat matA, struct double_cmat matB, struct double_cmat matC);
+
 static int matmul_double_sse2(struct double_cmat matA, struct double_cmat matB, struct double_cmat matC);
+
+static int matmul_double_strassen_windograd(struct double_cmat matA, struct double_cmat matB, struct double_cmat matC);
 
 #endif
