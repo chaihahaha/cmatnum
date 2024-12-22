d3d6d96d12508f043e77b20bb30c663f1e5d1fdb
chaihahaha <chai836275709@gmail.com>
2024-12-22 23:09:56 +0800

    add precision checking code

--- main.c
+++ main.c
@@ -38,6 +38,17 @@ int main() {
                           (end.tv_nsec - start.tv_nsec) / 1e9;
     printf("blas time: %f(s)\n", endtime);
 
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            if (fabs(C.data[i][j] - TC.data[i][j]) > 0.1) {
+                printf("wrong: %lf, true: %lf, at: %d-%d\n", C.data[i][j], TC.data[i][j], i, j);
+                return -1;
+            }
+        }
+    }
+    printf("finished\n");
+    return 0;
+
     free_double_matrix(A);
     free_double_matrix(B);
     free_double_matrix(C);
